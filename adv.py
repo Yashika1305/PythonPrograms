271ce09508e0435f29e6f4486aa2a98a32363adc
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-03-04 22:21:37 +0530

    Add files via upload

---
+++ adv.py
@@ -0,0 +1,10 @@
+#singleline multiple arguments
+a,b=[int(x) for x in input("please enter 2 values").split(' ')]
+print(a+b)
+
+#eval
+#a,b,c=[eval(x) for x in input("please enter the values").split(' ')]
+#s=eval("1+2+3+4+5")
+
+#s=eval("12**2")
+#print(s)
