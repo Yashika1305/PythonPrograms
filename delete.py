68cfa25734e7e54bbbf30f4e6234a2dbb0cb120e
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-03-06 19:43:47 +0530

    Add files via upload

---
+++ delete.py
@@ -0,0 +1,14 @@
+l=[2.3,4.9,3.5,4,0.76,1.02,1.2]
+print(l.pop())
+print(l.pop(5))
+print(l.pop(2))
+print(l)
+
+print('*****************')
+
+l=[1,2,3.4,2.5,6.3,7,4]
+del l[4]
+print(l)
+
+l.remove(2)
+print(l)
