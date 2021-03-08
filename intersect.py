3831f4cb30bac9c262448d28a5dabca290252b72
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-03-08 22:25:01 +0530

    Add files via upload

---
+++ intersect.py
@@ -0,0 +1,16 @@
+set1=set()
+set2=set()
+
+for i in range(5):
+	set1.add(i)
+
+for i in range(3,9):
+	set2.add(i)
+
+set3=set1.intersection(set2)
+print("intersection using function")
+print(set3)
+
+set3=set1 & set2
+print("intersection using'&' operator")
+print(set3)
