48d67e3dd3e044460b1eed9fea422535c83102de
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:50:17 +0530

    Add files via upload

---
+++ prog22.py
@@ -0,0 +1,24 @@
+dig=int(input("Input Digit(0-9) : "))
+if dig==0:
+	print("Zero")
+elif dig==1:
+	print("one")	  
+elif dig==2:
+	print("Two")	    
+elif dig==3:
+	print("Three\n")	       
+elif dig==4:
+	print("Four\n")
+elif dig==5:
+	print("Five\n");
+elif dig==6:
+	print("Six")	       
+elif dig==7:
+	printf("Seven")
+elif dig==8:
+	print("Eight")
+elif dig==9:
+	print("Nine")
+else:
+	print("invalid digit")
+	       
