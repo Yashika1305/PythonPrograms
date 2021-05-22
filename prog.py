f7c63df68d0a2468666aa425a925169c70aec1c0
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:47:01 +0530

    Add files via upload

---
+++ prog.py
@@ -0,0 +1,36 @@
+#marks=int(input("enter marks"))
+#print(("A grade") if marks>90 else print("B grade") if marks<=90 and marks>70 else print("C grade")
+#if marks<=70 and marks>50 else print("fail"))
+
+#a=int(input("Enter the value"))
+#b=int(input("Enter the value"))
+#print(print("a is equal to b") if a==b else print("a and b are unequal"))
+
+#a=int(input("Enter the value of integer"))
+#print("number is positive") if a>0 else print("NUmber is negative")
+
+#y=int(input("Enter the year"))
+#print("leap year") if y%4==0 else print("Not a leap year")
+
+#age=int(input("Enter the age"))
+#print("You can vote") if age>=18 else print("Sorry you can't vote")
+
+#m=int(input("Enter the value of m"))
+#print("n is 1") if m>0 else print("n is 0") if m==0 else print("n is -1")
+
+#height=int(input("Enter the height"))
+#print("The person is Dwarf") if height<150 else print("The person has Average height") if height==150 else print("The person is tall")
+
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+print("First number is greatest among all") if a>b and a>c else print("Second number is greatest among all") if b>a and b>c else print("Third number is greatest among all")
+
+#x=int(input("please enter the number"))
+#y=int(input("please enter the number"))
+#print("First Quadrant") if x>0 and y>0 else print("Second Quadrant") if x>0 and y<0 else print("Third Quadrant")
+
+#m=int(input("Enter marks in maths"))
+#p=int(input("Enter marks in physics"))
+#c=int(input("Enter marks in chemistry"))
+#print("Eligible for course") if (m>=65 and p>=55 and c>=50 and m+p+c>=180 and m+p>=140 and m+c>=140) else print("not eligible") 
