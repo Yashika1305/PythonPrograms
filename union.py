c86eebf83e634921d9cc9f2a53cad1d1fbb1138d
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:55:34 +0530

    Add files via upload

---
+++ union.py
@@ -0,0 +1,8 @@
+hello={"i","me","myself"}
+hi={"Yashika","Sharma"}
+hey=hello.union(hi)
+print("Union using union() function")
+print(hey)
+hey=hello|hi
+print("\n Union using '|' operator")
+print(hey)
