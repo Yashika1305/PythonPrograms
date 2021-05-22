cf379e425f3929f6d9500faeb0c53670b9c26cbf
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:49:41 +0530

    Add files via upload

---
+++ prog17.py
@@ -0,0 +1,5 @@
+al=input("Enter the alphabet")
+if (al=='a' or al=='e' or al=='i' or al=='o' or al=='u' or al=='A' or al=='I' or al=='O' or al=='U' or al=='E'):
+	print("Vowel")
+else:
+	print("Consonant")
