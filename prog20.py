4ffedefc26cb9ce15bca8937ab031a27da90a12b
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:49:59 +0530

    Add files via upload

---
+++ prog20.py
@@ -0,0 +1,11 @@
+g=input("Enter the grade E/V/G/A/F")
+if g=='E':
+	print("Excellent")
+elif g=='V':
+	print("Good")
+elif g=='A':
+	print("Average")
+elif g=='F':
+	print("Fail")
+else:
+	print("Invalid Grade")
