d3f338b4048a801797d5566ced48068a457a484c
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:50:06 +0530

    Add files via upload

---
+++ prog21.py
@@ -0,0 +1,17 @@
+i=int(input("Enter any integer(1-7)"))
+if i==1:
+	print("Day is Monday")
+elif i==2:
+	print("Day is Tuesday")
+elif i==3:
+	print("Day is Wednesday")
+elif i==4:
+	print("Day is Thursday")
+elif i==5:
+	print("Day is Friday")
+elif i==6:
+	print("Day is Saturday")
+elif i==7:
+	print("Day is Sunday")
+else:
+	print("Invalid i")
