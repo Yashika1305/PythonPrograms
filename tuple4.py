37931eea6621b661b903ed2ba1f1fbdca23dfa09
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:55:28 +0530

    Add files via upload

---
+++ tuple4.py
@@ -0,0 +1,17 @@
+#t=(1,2,3,4,5)
+#print(max(t))
+#print(min(t))
+
+t1=('python','core')
+t2=('run',2)
+if (cmp(t1,t2)!=0):
+	print("not the ssame")
+else:
+	print('same')
+print('maximum element in tuple 1,2:'+str(max(tuple1)) +  ',' + 
+        str(max(tuple2))) 
+print ('Minimum element in tuples 1,2: ' + 
+     str(min(tuple1)) + ','  + str(min(tuple2))) 
+
+
+
