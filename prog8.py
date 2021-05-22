dbbe100de6cd54706ff258f122d8c9d4b5238ce6
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:48:16 +0530

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+if a>b and a>c:
+	print("First number is greatest among all")
+elif b>a and b>c:
+	print("Second number is greatest among all")
+else:
+	print("Third number is greatest among all")
