ffdebc5404a95d5e8b36b19399a6a0e9cc181d28
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:54:21 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,13 @@
+#myset = set(["a", "b", "c"]) 
+#print(myset) 
+#myset.add("d") 
+#print(myset) 
+
+people={"Yashika","Samarth","Anubhav"}
+print("people:",end=" ")
+print(people)
+people.add("python")
+for i in range(1,6):
+	people.add(i)
+print("\nSet after adding element:",end=" ")
+print(people)
