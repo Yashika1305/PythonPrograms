c875c2e30fd13f6a6b31ec76a2be1968372c709d
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:50:29 +0530

    Add files via upload

---
+++ prog23.py
@@ -0,0 +1,27 @@
+m=int(input("enter the month in integer(1-12):"))
+if m==1:
+	print("January")
+elif m==2:
+	print("Febrary")
+elif m==3:
+	print("march")
+elif m==4:
+	print("April")
+elif m==5:
+	print("May")
+elif m==6:
+	print("June")
+elif m==7:
+	print("July")
+elif m==8:
+	print("August")
+elif m==9:
+	print("September")
+elif m==10:
+	print("October")
+elif m==11:
+	print("November")
+elif m==12:
+	print("December")
+else:
+	print("try again")
