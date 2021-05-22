4520aaa294dd4f5d9f7c6cf5235bb67f94020312
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:48:56 +0530

    Add files via upload

---
+++ prog15.py
@@ -0,0 +1,8 @@
+a1=int(input("Enter angle one"))
+a2=int(input("Enter angle two"))
+a3=int(input("Enter angle three"))
+sum=a1+a2+a3
+if sum==180:
+	print("Triangle is valid")
+else:
+	print("Triangle is not valid")
