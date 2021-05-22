f0f14006e27bd50efe75980cec734f039bc8b8d5
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:46:39 +0530

    Add files via upload

---
+++ pr5.py
@@ -0,0 +1,18 @@
+class stud:
+	def ma(self):
+
+		name=input("Enter the name")
+		rn=int(input("Enter th rollno"))
+class marks:
+	def mb(self):
+		a=stud()
+		a.ma()
+
+		m=int(input("enter marks in maths"))
+		p=int(input("enter marks in physics"))
+		c=int(input("enter marks in chem"))
+		e=int(input("enter marks in eng"))
+		h=int(input("enter marks in hindi"))
+		
+b=marks()
+b.mb()
