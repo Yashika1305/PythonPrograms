05332853c8062423dd653f1ac209bebb5d5362a5
Yashika1305 <71085373+Yashika1305@users.noreply.github.com>
2021-05-22 23:46:32 +0530

    Add files via upload

---
+++ pr4.py
@@ -0,0 +1,11 @@
+l=[1,2,4,78,56,43,9]
+l1=[]
+print(min(l))
+l1.append(min(l))
+l1.append(min(l))
+l1.append(min(l))
+l1.append(min(l))
+l1.append(min(l))
+l1.append(min(l))
+l1.append(min(l))
+print(l1)
